9af6a4269f9822f56d16d02f6a3b7687c14ecfb6
KSY <esther.soyoung@gmail.com>
2020-07-03 00:27:12 +0900

    formatting

--- filter_visualization/FilterViz.py
+++ filter_visualization/FilterViz.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
 
 import math
 import numpy as np
@@ -13,24 +10,38 @@ from io import BytesIO
 
 class FilterViz():
 
-    def __init__(self, model):
-        self._model = model
-        #self._data_shape = data_shape
-        #self._num_class = 10
+    def __init__(self):
+        self._model = load_model('models/mnist_model.h5')
+        self._img_rows = 28
+        self._img_cols = 28
+        (self._x_train, y_train), (self._x_test, y_test) = mnist.load_data()
+
+        if K.image_data_format() == 'channels_first':
+            self._x_train = self._x_train.reshape(self._x_train.shape[0], 1, self._img_rows, self._img_cols)
+            self._x_test = self._x_test.reshape(self._x_test.shape[0], 1, self._img_rows, self._img_cols)
+        else:
+            self._x_train = self._x_train.reshape(self._x_train.shape[0], self._img_rows, self._img_cols, 1)
+            self._x_test = self._x_test.reshape(self._x_test.shape[0], self._img_rows, self._img_cols, 1)
+
+        self._x_train = self._x_train.astype('float32')
+        self._x_test = self._x_test.astype('float32')
+        self._x_train /= 255
+        self._x_test /= 255
 
     def _get_hidden_layers(self, data):
-        feature_extractor = keras.Model(inputs=self._model.inputs, \
+        feature_extractor = tf.keras.Model(inputs=self._model.inputs, \
                             outputs=[layer.output for layer in self._model.layers[:-4]])
         image = tf.convert_to_tensor(np.expand_dims(data, axis=0))
         return feature_extractor(image)
 
     def _plot_filter(self, feature):
         feature = K.eval(feature)
-        filters = feature.shape[3] 
-        plt.figure(1, figsize=(20,20))
-        n_columns = 5
+        #filters = feature.shape[3] 
+        filters = 18
+        plt.figure(1, figsize=(8,15))
+        n_columns = 3
         n_rows = math.ceil(filters / n_columns) + 1
-        for i in range(filters):
+        for i in range(6, filters):
             plt.subplot(n_rows, n_columns, i+1)
             plt.imshow(feature[0,:,:,i], interpolation='nearest')
         img = BytesIO()
@@ -38,13 +49,14 @@ class FilterViz():
         img.seek(0)
         return img
 
+    '''
+    data: (28, 28)
+    layer range: 0, 1, 2, 3
+    '''
     def get_FilterViz(self, data, layer):
-        # reshape
+        data = data.reshape(self._img_rows, self._img_cols, 1)
         data = data.astype('float32')
         data /= 255
 
         hiddens = self._get_hidden_layers(data)
-        print(hiddens[layer].shape)
         return self._plot_filter(hiddens[layer])
-        # for layer in range(len(hiddens)):
-        #     yield self._plot_filter(hiddens[layer])
